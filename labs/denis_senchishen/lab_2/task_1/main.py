7d5e12dad37bc218af6fa3a4d611ac4243f0f119
Denis Senchishen <dsenchishen@icloud.com>
2021-02-09 21:31:33 +0200

    Unittest sample

---
+++ labs/denis_senchishen/lab_2/task_1/main.py
@@ -0,0 +1,8 @@
+def sum_two_numbers(arg1, arg2):
+    return arg1 + arg2
+
+
+if __name__ == '__main__':
+    number_1 = int(input("Number 1: "))
+    number_2 = int(input("Number 2: "))
+    print(sum_two_numbers(number_1, number_2))
