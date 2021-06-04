2314725b89cc0df86aa7850326acee4f3d043188
Vitaliy Tolochniy <vitaliyreix@gmail.com>
2021-06-04 13:57:10 +0300

    Лабораторная 10 upd

--- labs/vitaliy_tolochniy/lab10/1.py
+++ labs/vitaliy_tolochniy/lab10/1.py
@@ -5,32 +5,32 @@
 Структура запису - прізвище, рік народження, стаж роботи, оклад. Розробити програму, що за
 прізвищем визначає стаж роботи."""
 
-def findYears(workers1, sur):
-    for i in workers1:
+def findYears(workers, sur):
+    for i in workers:
         if i.surname == sur:
             return i.yearsOfWork
     
-class workers:
+class Worker:
     surname = ""
     yearOfBirth = 0
     yearsOfWork = 0
-    Salary = 0
+    salary = 0
 
-    def __init__(self, surname, yearOfBirth, yearsOfWork, Salary ):
+    def __init__(self, surname, yearOfBirth, yearsOfWork, salary ):
         self.surname = surname 
         self.yearOfBirth = yearOfBirth
         self.yearsOfWork = yearsOfWork
-        self.Salary = Salary
+        self.salary = salary
         
-workers1 = []
+workers = []
 a = int(input("Сколько рабочих вы хотите ввести?: "))
 print("Входные данные в формате: фамилия, год рождения, годы работы, заработная плата: ")
 for i in range(a):
     l1 = [j for j in input().split()]
-    worker = workers(l1[0],l1[1],l1[2],l1[3])
-    workers1.append(worker)
+    worker = Worker(l1[0],l1[1],l1[2],l1[3])
+    workers.append(worker)
 
 sur = input("Введите фамилию или 0, если вы закончили: ")
 while(sur != "0"):
-    print(findYears(workers1, sur))
+    print(findYears(workers, sur))
     sur = input("Введите фамилию или 0, если вы закончили: ")
